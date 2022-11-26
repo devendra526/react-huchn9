74451870b06587a6c8d68aef64f498b9a86ad515
devendra526 <39769296+devendra526@users.noreply.github.com>
2022-11-26 20:08:07 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -11,13 +11,18 @@ export const GlobalData = createContext();
 
 export default function App() {
   const [color, setColor] = useState('green');
+  const [day, setDay] = useState("Monday")
   const changeColor = () => {
      setColor((color)=>"red")
   }
+  const getDay = () => {
+      setDay((day)=>"Sunday")
+  }
   return (
     <div>
-      <GlobalData.Provider value={{ textColor: color }}>
-        <h2>This is App Component</h2>
+      <GlobalData.Provider value={{ textColor: color, getDay:getDay }}>
+        <h2>This is App Component </h2>
+        <h2>Today is {day} </h2>
         <Child />
         <Other />
         <button onClick={changeColor}>Change Color</button>
--- src/SuperChild.js
+++ src/SuperChild.js
@@ -2,10 +2,12 @@ import React, { useContext } from 'react';
 import { GlobalData } from './App';
 
 const SuperChild = () => {
-  const { textColor } = useContext(GlobalData);
+  const { textColor, getDay } = useContext(GlobalData);
+  const day = 'Sunday';
   return (
     <div>
       <h2 style={{ color: textColor }}>This is Super Child component</h2>
+      <button onClick={() => getDay(day)}>Click Me</button>
     </div>
   );
 };
