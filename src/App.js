f0b98d450babb69914db79ba40ae069ebec6982b
devendra526 <39769296+devendra526@users.noreply.github.com>
2022-11-26 13:51:23 +0530

    1 file modified, 3 deleted

--- src/App.js
+++ src/App.js
@@ -2,27 +2,11 @@ import React from 'react';
 import './style.css';
 import UseEffectExample from './UseEffectExample';
 import ApiExample from './ApiExample';
-import Home from './Route/Home';
-import About from './Route/About';
-import Contact from './Route/Contact';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />}>
-            Home
-          </Route>
-          <Route path="/about" element={<About />}>
-            About
-          </Route>
-          <Route path="/contact" element={<Contact />}>
-            Contact
-          </Route>
-        </Routes>
-      </Router>
+      <ApiExample />
     </div>
   );
 }
